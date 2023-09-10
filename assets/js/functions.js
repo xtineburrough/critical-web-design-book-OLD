e43a7d539111ce3c47ec06e85a75d38f10f39287
Owen Mundy <omundy@gmail.com>
2023-09-10 16:43:24 -0400

    Update functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -17,8 +17,44 @@ function randomInt(min = 0, max = 1) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 /**
+ *  Return a random index from the array
+ */
+function randomFromArray(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
+
+/**
  *  Capitalize first letter of string. Credit: https://stackoverflow.com/a/1026087/441878
  */
 function upperCaseFirst(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ *  Zero-pad a string
+ */
+function zeroPad(str) {
+	return ("0" + str).slice(-2);
+}
+
+/**
+ *  Convert a date to a timezone
+ */
+function convertTZ(date, tzString) {
+	return new Date(
+		(typeof date === "string" ? new Date(date) : date).toLocaleString(
+			"en-US",
+			{ timeZone: tzString }
+		)
+	);
+}
+
+/**
+ *  Populate an array with a string
+ */
+function populateArr(str, count) {
+	var sizedArray = Array.apply(null, Array(count));
+	return sizedArray.map(function (o) {
+		return str;
+	});
 }
