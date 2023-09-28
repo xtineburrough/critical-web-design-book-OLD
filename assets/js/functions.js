f2e23d708bc71d8508df11e2076cce3f4adcb52f
Owen Mundy <omundy@gmail.com>
2023-09-28 17:27:43 -0400

    Add accessibility-data example

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -58,3 +58,13 @@ function populateArr(str, count) {
 		return str;
 	});
 }
+
+/**
+ *  Shuffle an array - credit https://stackoverflow.com/a/12646864/441878
+ */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
